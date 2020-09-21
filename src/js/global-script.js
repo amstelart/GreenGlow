d5838a957073ae3e163993fd03683dc2bb2b6d5b
amstelart <amsteldroid@gmail.com>
2020-09-21 08:12:52 +0300

    dev-1

--- src/js/global-script.js
+++ src/js/global-script.js
@@ -1,7 +1,51 @@
 // Если на проекте jQuery
-// $( document ).ready(function() {
-//   // code
-// });
+$( document ).ready(function() {
+  $('select').niceSelect();
+  // dropdown
+  $('.js-drop-toggle').click(function(){
+    var parent = $(this).closest(".js-drop-wrap");
+    var current = parent.find(".js-drop-cont");
+    current.slideToggle();
+    parent.toggleClass("active")
+  });
+  // dropdown === end
+  // code
+  // code
+  // code
+  // code
+  // code
+  // code
+});
+
+$(window).on('load resize', function() {
+    if ($(window).width() < 768) {
+      $('#team-carousel:not(.slick-initialized)').slick({
+        centerMode: false,
+        variableWidth: true,
+        dots: true,
+        infinite: false,
+        arrows: false,
+        speed: 100,
+        slidesToShow: 2
+      });
+    } else {
+      $("#team-carousel.slick-initialized").slick("unslick");
+    }
+
+    if ($(window).width() < 768) {
+      $('#caption-carousel:not(.slick-initialized)').slick({
+        centerMode: false,
+        variableWidth: true,
+        dots: true,
+        infinite: false,
+        arrows: false,
+        speed: 100,
+        slidesToShow: 1
+      });
+    } else {
+      $("#caption-carousel.slick-initialized").slick("unslick");
+    }
+  });
 
 // Изоляция без jQuery
 // (function(){
