f63404aeec228f0df5e585f9318f60202bd02f73
amstelart <amsteldroid@gmail.com>
2020-09-23 08:08:28 +0300

    dev-4

--- src/js/global-script.js
+++ src/js/global-script.js
@@ -10,7 +10,6 @@ $( document ).ready(function() {
     parent.toggleClass("active")
   });
   // dropdown === end
-  // code
   var wow = new WOW(
     {
       boxClass:     'wow',      // animated element css class (default is wow)
@@ -27,11 +26,6 @@ $( document ).ready(function() {
     }
   );
   wow.init();
-  // code
-  // code
-  // code
-  // code
-  // code
 });
 
 $(window).on('load resize', function() {
@@ -39,7 +33,7 @@ $(window).on('load resize', function() {
       $('#team-carousel:not(.slick-initialized)').slick({
         centerMode: false,
         variableWidth: true,
-        dots: true,
+        dots: false,
         infinite: false,
         arrows: false,
         speed: 100,
@@ -53,7 +47,7 @@ $(window).on('load resize', function() {
       $('#caption-carousel:not(.slick-initialized)').slick({
         centerMode: false,
         variableWidth: true,
-        dots: true,
+        dots: false,
         infinite: false,
         arrows: false,
         speed: 100,
@@ -67,7 +61,7 @@ $(window).on('load resize', function() {
       $('#post-carousel:not(.slick-initialized)').slick({
         centerMode: false,
         variableWidth: true,
-        dots: true,
+        dots: false,
         infinite: true,
         arrows: false,
         speed: 100,
@@ -78,17 +72,6 @@ $(window).on('load resize', function() {
     }
   });
 
-  // var image = document.getElementsByClassName('parallax');
-  // new simpleParallax(image, {
-  //   delay: .6,
-  //   transition: 'cubic-bezier(0,0,0,1)'
-  // });
-
-// Изоляция без jQuery
-// (function(){
-//   // code
-// }());
-
 // На проекте нет jQuery, но хочется $( document ).ready...
 function ready(fn) {
   if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading"){
@@ -97,7 +80,147 @@ function ready(fn) {
     document.addEventListener('DOMContentLoaded', fn);
   }
 }
-//
-// ready(function(){
-//   // code
-// });
+
+// zeynepjs initialization for demo
+$(function() {
+  // loop all zeynepjs menus for initialization
+  $('.zeynep').each(function () {
+    // init zeynepjs side menu
+    $(this).zeynep({
+      opened: function (el) {
+        // log
+        console.log(el.attr('data-menu-name') + ' side menu opened')
+      },
+      closed: function (el) {
+        // log
+        console.log(el.attr('data-menu-name') + ' side menu closed')
+      }
+    })
+  })
+
+  // handle zeynepjs overlay click
+  $('.zeynep-overlay').on('click', function () {
+    // close all zeynepjs menus
+    $('.zeynep.opened').each(function () {
+      $(this).data('zeynep').close()
+    })
+  });
+  $('.zeynep__close').on('click', function () {
+    // close all zeynepjs menus
+    $('.zeynep.opened').each(function () {
+      $(this).data('zeynep').close()
+    })
+  });
+
+  // open first zeynepjs side menu
+  $('#main-nav-toggler').on('click', function () {
+    $('.zeynep.left').data('zeynep').open();
+  });
+  
+  // open second zeynepjs side menu
+  $('#basket-toggler').on('click', function () {
+    $('.zeynep.right').data('zeynep').open();
+  });
+});
+
+/**
+ * =================================================================================
+ *
+ * PLON Component : ScrollParallax
+ *
+ * @author			Bartosz Perończyk (peronczyk.com)
+ * @modified		2017-09-15
+ * @repository		https://github.com/peronczyk/plon
+ *
+ * =================================================================================
+ */
+
+
+(function($) {
+
+	'use strict';
+
+	/** ----------------------------------------------------------------------------
+	 * PLUGIN DEFAULT CONFIGURATION
+	 */
+
+	var defaults = {
+			debug: 0,
+			dataSelector: 'data-parallax',
+			defaultSpeed: 0.7,
+			eventsNamespace: '.plon.scrollparallax',
+		},
+		$document 		= $(document),
+		frameRequested	= false,
+		offset, speed;
+
+
+	/** ----------------------------------------------------------------------------
+	 * ADJUST PARALLAX LAYERS
+	 */
+
+	function paralaxAdjust(config, layers) {
+		for (var n = 0 ; n < layers.length ; n++) {
+			offset = $document.scrollTop() * layers[n].speed;
+			layers[n].obj.css({transform: 'translate3d(0,' + offset + 'px,0)'});
+		}
+	}
+
+
+	/** ----------------------------------------------------------------------------
+	 * SET UP JQUERY PLUGIN
+	 */
+
+	$.scrollParallax = function(options) {
+
+		// Setup configuration
+		var config = $.extend({}, defaults, options);
+
+		// Definitions
+		var layers = [],
+			_self = $('[' + config.dataSelector + ']'),
+			elem,
+			layersCount = 0;
+
+		if (config.debug) console.info('Plugin loaded: scrollParallax');
+
+		// Create array of layers
+		_self.each(function(index) {
+			elem = $(this);
+			speed = elem.attr(config.dataSelector);
+
+			layers[index] = {
+				'obj'	: elem,
+				'speed'	: (speed) ? parseFloat(speed) : config.defaultSpeed,
+			}
+			layersCount++;
+		});
+
+		if (console.debug) console.info('scrollParallax: layers found - ' + layersCount);
+
+		paralaxAdjust(config, layers);
+
+		// Monitor document scrolling
+		$(window).on('scroll' + config.eventsNamespace, function() {
+			if (frameRequested) return;
+			frameRequested = true;
+			requestAnimationFrame(function() {
+				paralaxAdjust(config, layers);
+				frameRequested = false;
+			});
+		});
+	}
+
+})(jQuery);
+
+var debug = 1; // Debug mode
+
+$(function() {
+
+	'use strict';
+
+	$.scrollParallax({
+		debug: debug,
+	});
+
+});
